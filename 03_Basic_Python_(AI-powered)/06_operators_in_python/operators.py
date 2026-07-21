19bbbb451a8478253bdd8aca5a9611592b90777b
Shubham Chaudhary <shubhamyadav9274@gmail.com>
2026-07-21 14:19:33 +0530

    update operators.py in section 03

---
+++ 03_Basic_Python_(AI-powered)/06_operators_in_python/operators.py
@@ -0,0 +1,54 @@
+# ********************************** OPERATORS **********************************
+# Operators - Operators are special symbols in python that perform specific operations on one or more operands (values or variables). They are fundamental to programmins because they allow you to manipulate data, make decisions, and control program flow.
+
+# In simple words + , -, *, / are operators 
+# Example 
+# 1). 
+a = 2
+b = 6
+print(a + b)
+                # Here , 
+                #      a and b are operands
+                #      + are operators
+
+
+# 2).
+x = 5
+y = 6
+print(y - x)
+                # Here , 
+                #      x and y are operands
+                #      - are operators
+
+
+# 3).
+A = 23
+B = 2
+print(A * B)
+                # Here , 
+                #      A and B are operands
+                #      * are operators
+
+
+# 4).
+X = 25
+Y = 5
+print(X / Y)
+                # Here , 
+                #      X and Y are operands
+                #      / are operators
+
+
+
+# Why they matters ? 
+# - Every calculation, comparision, or logical decision in python relies on operators working correctly
+
+
+# ***************** THERE ARE MULTIPLES CATEGORIES OF OPERATORS *****************
+# 1). Arithmatic Operators - Use for msath calculations.
+# 2). Comparison Operators - Use for Relational checks
+# 3). Assignment Operators - Use for Storing values
+# 4). Logical Operators - Use for Boolean logic
+# 5). Membership Operators - Use for Sequence checks
+# 6). Identity Operators - Use for Object comparison
+# 7). Bitwise Operators - Use for Binary operations
